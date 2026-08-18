f83ae019bdc53bd5d622d9c205c6e1aeeee2439e
Ziramog <fullpowerok@gmail.com>
2026-08-17 21:11:31 -0300

    content: update hero text to Negocios Inmobiliarios Urbanos y Rurales

--- utils/getSiteConfig.js
+++ utils/getSiteConfig.js
@@ -3,7 +3,7 @@ import SiteConfig from '@/models/SiteConfig';
 import { SITE_CONFIG } from '@/config/site';
 
 export const DEFAULT_SITE_CONFIG = {
-  heroTitle: 'Propiedades seleccionadas, asesoramiento de confianza',
+  heroTitle: 'Negocios Inmobiliarios Urbanos y Rurales',
   heroSubtitle: 'Farias & Asociados',
   aboutTitle: 'Farias & Asociados',
   aboutSubtitle: 'SERVICIOS INMOBILIARIOS',
